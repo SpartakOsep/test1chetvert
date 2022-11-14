ebff7c7351eb713922a7e5c40a216052d96c2435
Spartak <spartak.os@mail.ru>
2022-11-14 22:53:28 +0300

    процесс

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] arrayone = new string[7] { "hello", "2", "world", "1234", "1567", "-2", "computer science"};
+string[] arraytwo = new string[arrayone.Length];
+void ArrayWith(string[] arrayone, string[] arraytwo)
+{
+    int count = 0;
+    for (int i = 0; i < arrayone.Length; i++)
+    {
+        if (arrayone[i].Length <= 3)
+        {
+            arraytwo[count] = arrayone[i];
+            count++;
+        }
+    }
+}
